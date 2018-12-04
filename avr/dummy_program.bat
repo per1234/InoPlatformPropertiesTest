c4e4e31af796d939f7d26b74dc7ed9bf273ae23d
per1234 <accounts@perglass.com>
2018-12-03 18:35:44 -0800

    Add support for Linux and macOS
    Untested

---
+++ avr/dummy_program.bat
@@ -0,0 +1,2 @@
+@ECHO OFF
+REM Intentionally left empty so that the recipes in platform.txt have a command to run that will not error
---
+++ avr/dummy_program.sh
@@ -0,0 +1,1 @@
+# Intentionally left empty so that the recipes in platform.txt have a command to run that will not error
